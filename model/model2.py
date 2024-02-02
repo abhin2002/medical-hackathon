a6373f0bc2024255dd07972b8a72a69b265c503f
Abhishek0075 <abhirajp075@gmail.com>
2024-02-02 14:44:56 +0530

    model for image-2-text created

---
+++ model/model2.py
@@ -0,0 +1,10 @@
+from transformers import AutoModelForCausalLM, CodeGenTokenizerFast as Tokenizer
+from PIL import Image
+
+model_id = "vikhyatk/moondream1"
+model = AutoModelForCausalLM.from_pretrained(model_id, trust_remote_code=True)
+tokenizer = Tokenizer.from_pretrained(model_id)
+
+image = Image.open(r'C:\Users\itsab\Desktop\hackthon\huggingFace\dead.jpg')
+enc_image = model.encode_image(image)
+print(model.answer_question(enc_image, "give detail on this accident situtaion", tokenizer))
