da3e68d7e75e65e5120049d1c0db5c751d489c11
devcb13d <devcb@pg.cusat.ac.in>
2024-02-02 15:49:32 +0530

    Add model

--- model/model2.py
+++ model/model2.py
@@ -1,10 +1,21 @@
 from transformers import AutoModelForCausalLM, CodeGenTokenizerFast as Tokenizer
 from PIL import Image
 
+from ..rag.rag import load_rag_pipeline, answer_question
+
 model_id = "vikhyatk/moondream1"
 model = AutoModelForCausalLM.from_pretrained(model_id, trust_remote_code=True)
 tokenizer = Tokenizer.from_pretrained(model_id)
 
 image = Image.open(r'C:\Users\itsab\Desktop\hackthon\huggingFace\dead.jpg')
 enc_image = model.encode_image(image)
-print(model.answer_question(enc_image, "give detail on this accident situtaion", tokenizer))
+
+prompt = "You are an emergency assistant and you have to give detail on this accident situtaion in the image."
+response = model.answer_question(enc_image,prompt, tokenizer)
+
+print(response)
+
+# Get the recommended first aid
+qa = load_rag_pipeline()
+ans = answer_question(response,qa)
+
